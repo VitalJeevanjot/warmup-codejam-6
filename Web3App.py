bafefa95b4a3332a7f527070c4e047c476a877d4
sandhan26 <sandhan.sarma26@gmail.com>
2020-01-18 00:23:11 +0530

    Added buttons to open transaction url via default web browser

--- Web3App.py
+++ Web3App.py
@@ -6,9 +6,24 @@ from kivy.loader import Loader
 from kivy.clock import Clock
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.recycleview import RecycleView
+from kivy.properties import ColorProperty
+from kivy.properties import StringProperty
+import webbrowser
 
 my_provider = Web3.HTTPProvider('https://rpc.astor.host/node')
 w3 = Web3(my_provider)
+
+class CustomLabel(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(CustomLabel, self).__init__(**kwargs)
+        label_text = StringProperty(None)
+        transaction_id = StringProperty(None)
+        label_color = ColorProperty(None)
+
+    def open_url(self, transaction_id):
+        webbrowser.open_new_tab('https://blockscout.funcoin.io/tx/'+transaction_id+'/internal_transactions')
+
 
 class Web3Widget(BoxLayout):
     pass
@@ -35,22 +50,29 @@ class MainWidget(App):
     def set_list(self, req, result):
         # Add loading here
         if result['result'] == None:
-            self.transactions.data = [{"color": (.5, .5, 1, 1), 'text': str('No transaction for this input box address')}]
+            self.transactions.data = [
+                {"label_color": (.5, .5, 1, 1),
+                 'label_text': str('No transaction for this input box address'),
+                 'transaction_id':''
+                 }
+            ]
         else:
             self.transactions.data = []
             for transaction in result['result']:
-                if(self.current_address == transaction['to']):
+                if(self.current_address.lower() == transaction['to'].lower()):
                     self.transactions.data.append(
                         {
-                            "color":(0,1,0,1),
-                            "text":str(transaction['value'])
+                            "label_color":(0,1,0,1),
+                            "label_text":str(transaction['value']),
+                            "transaction_id":transaction['hash']
                         }
                     )
                 else:
                     self.transactions.data.append(
                         {
-                            "color": (1, 0, 0, 1),
-                            "text": str(transaction['value'])
+                            "label_color": (1, 0, 0, 1),
+                            "label_text": str(transaction['value']),
+                            "transaction_id": transaction['hash']
                         }
                     )
         pass
@@ -58,7 +80,12 @@ class MainWidget(App):
     def on_error(self, req, error):
         print('error')
         print(error)
-        self.transactions.data = [{"color": (.5, .5, 1, 1), 'text': str(error)}]
+        self.transactions.data = [
+            {"label_color": (.5, .5, 1, 1),
+             'label_text': str(error),
+             "transaction_id":''
+             }
+        ]
         pass
         # App.get_running_app().root.ids.get_values.text
 

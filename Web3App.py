283953eaf544793a0de0eb0d4e1788de2c3d2192
tienloc <tienlocnguyen.ac@gmail.com>
2020-01-19 00:25:42 +0700

    Add loading effect

--- Web3App.py
+++ Web3App.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from web3 import Web3
 from kivy.clock import Clock
@@ -10,10 +11,12 @@ from kivy.uix.recycleview import RecycleView
 from kivy.properties import ColorProperty
 from kivy.properties import StringProperty
 import webbrowser
+from kivy.uix.popup import Popup
+
+from loading_widget import LoadingWidget, LoadingWidgets
 
 my_provider = Web3.HTTPProvider('https://rpc.astor.host/node')
 w3 = Web3(my_provider)
-
 
 BLOCK_SCOUT_URL = "https://blockscout.funcoin.io/api?module=account&action=txlist&address={}"
 LAST_SEARCH_FILE = Path(__file__).parent / 'last_search.txt'
@@ -30,26 +33,28 @@ class CustomLabel(BoxLayout):
         webbrowser.open_new_tab('https://blockscout.funcoin.io/tx/'+transaction_id+'/internal_transactions')
 
 
-class Web3Widget(BoxLayout):
+class Web3Widget(FloatLayout):
     pass
 
 
 class RV(RecycleView):
     pass
 
+class MyPopup(Popup):
+    pass
 
 class MainWidget(App):
     # print(w3.eth.filter('pending').get_new_entries())
     transactions = []
-    
     last_search = ''
 
     def build(self):
-        root_widget = Web3Widget()
-        return root_widget
+        self.search_popup = MyPopup()
+        self.root_widget = Web3Widget()
+        return self.root_widget
 
     def get_data(self, address, rsv):
-
+        self.search_popup.ids['loading_widget'].start()
         req = UrlRequest(url=BLOCK_SCOUT_URL.format(address.text),
                          on_success=self.set_list,
                          on_failure=self.on_error,
@@ -59,6 +64,7 @@ class MainWidget(App):
 
     def set_list(self, req, result):
         # Add loading here
+        self.search_popup.ids['loading_widget'].stop()
         if result['result'] == None:
             self.transactions.data = [
                 {"label_color": (.5, .5, 1, 1),
@@ -106,15 +112,37 @@ class MainWidget(App):
                 self.last_search = f.read()
 
     def on_stop(self):
+        self.search_popup.ids['loading_widget'].stop()
         with open(LAST_SEARCH_FILE, 'w') as f:
             f.write(self.last_search)
+        pass
+        # App.get_running_app().root.ids.get_values.text
+
+    def start_loading(self):
+        self.root_widget.ids['loading_widget'].start()
+
+    def stop_loading(self):
+        self.root_widget.ids['loading_widget'].stop()
+
+    def show_popup(self):
+        self.search_popup.open()
+
+    def update_chain_index(self):
+        self.start_loading()
+        Clock.schedule_once(self._get_chain_index, 1/30.)
+
+    def _get_chain_index(self, dt):
+        chain_index = str(w3.eth.blockNumber)
+        self.stop_loading()
+        self.root.ids.blocknom.text = chain_index
 
 
 def get_chain_index(dt):
     App.get_running_app().root.ids.blocknom.text = str(w3.eth.blockNumber)
 
 
-Clock.schedule_once(get_chain_index, 1)
+Clock.schedule_once(get_chain_index, 5)
 
+Builder.load_file('loading_widget.kv')
 Builder.load_file('Web3.kv')
 MainWidget().run()
---
+++ loading_widget.py
@@ -0,0 +1,52 @@
+import random
+
+from kivy.animation import Animation
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty
+
+
+class LoadingWidget(Widget):
+    def set_alpha_channel(self, opacity):
+        self.color = [self.color[i] for i in range(3)] + [opacity]
+
+
+class LoadingWidgets(Widget):
+    child_number = NumericProperty()
+    def __init__(self, **kwargs):
+        super(LoadingWidgets, self).__init__(**kwargs)
+        self.child_number = kwargs.get('child_number', 5)
+        for i in range(1, self.child_number + 1):
+            widget = LoadingWidget()
+            widget.opacity = 0
+            widget.set_alpha_channel(i/4)
+            self.add_widget(widget)
+        self.anims = []
+        self.animating = False
+    
+    def start(self):
+        i = 1
+        self.animating = True
+        for widget in self.children:
+            widget.opacity = 100
+            anim = Animation(x=self.pos[0] + 200, y=self.pos[1], 
+                            duration=self._anim_duration(i), t='in_circ') + \
+                   Animation(x=self.pos[0], y=self.pos[1], 
+                            duration=self._anim_duration(i), t='in_circ')
+            anim.bind(on_complete=self._on_complete)
+            anim.repeat = True
+            self.anims.append(anim)
+            anim.start(widget)
+            i += 1
+
+    def stop(self):
+        for anim in self.anims:
+            anim.repeat = False
+        self.animating = False
+
+    def _on_complete(self, animation, widget):
+        if not self.animating:
+            widget.opacity = 0
+            widget.pos = self.pos
+
+    def _anim_duration(self, i):
+        return i/(self.child_number^2) + 0.5

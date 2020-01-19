781d83d4b771a94e083026b69fdee4cf418ea72b
tienloc <tienlocnguyen.ac@gmail.com>
2020-01-19 10:24:02 +0700

    temp

--- loading_widget.py
+++ loading_widget.py
@@ -3,6 +3,8 @@ import random
 from kivy.animation import Animation
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ObjectProperty
+from kivy.logger import Logger
+
 
 
 class LoadingWidget(Widget):
@@ -15,11 +17,13 @@ class LoadingWidgets(Widget):
 
     def __init__(self, **kwargs):
         super(LoadingWidgets, self).__init__(**kwargs)
+        Logger.info(f'Loading widgets pos: {self.pos}, size: {self.size}')
         self.child_number = kwargs.get('child_number', 5)
         for i in range(1, self.child_number + 1):
-            widget = LoadingWidget()
-            widget.opacity = 0
+            widget = LoadingWidget(center=self.center, pos_hint={'x':1, 'y':.5})
+            # widget.opacity = 0
             widget.set_alpha_channel(i/4)
+            Logger.info(f'Widget #{i} pos: {widget.pos}, size: {widget.size}')
             self.add_widget(widget)
         self.anims = []
         self.animating = False
@@ -29,7 +33,7 @@ class LoadingWidgets(Widget):
         self.animating = True
         for widget in self.children:
             widget.opacity = 100
-            anim = Animation(x=self.pos[0] + 200, y=self.pos[1],
+            anim = Animation(x=self.pos[0] + self.width, y=self.pos[1],
                              duration=self._anim_duration(i), t='in_circ') + \
                 Animation(x=self.pos[0], y=self.pos[1],
                           duration=self._anim_duration(i), t='in_circ')
